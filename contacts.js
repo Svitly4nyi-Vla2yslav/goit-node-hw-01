90b37aa82dfb6f5bf257f3c4a381a73a77bd4042
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-11-10 23:47:22 +0100

    addContact

--- contacts.js
+++ contacts.js
@@ -1,35 +1,49 @@
 const fs = require("fs/promises");
 const path = require("path");
+const {nanoid} = require("nanoid")
+const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-const contactsPath = path.join(__dirname, "contacts.json");
-
- 
+console.log(__dirname)
 
 // TODO: задокументувати кожну функцію
 async function listContacts() {
-  try { const data = await fs.readFile(contactsPath, "utf-8" )
-  return JSON.parse(data);
-} catch (error) {
-  console.log(error)
+ 
+    const data = await fs.readFile(contactsPath, "utf-8")
+    return JSON.parse(data);
+ 
+  // ...твій код. Повертає масив контактів.
 }
-    // ...твій код. Повертає масив контактів.
-  }
-  
-  function getContactById(contactId) {
-    // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-  }
-  
-  function removeContact(contactId) {
-    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-  }
-  
-  function addContact(name, email, phone) {
-    // ...твій код. Повертає об'єкт доданого контакту. 
-  }
 
-  module.exports = {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact,
-  }
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === contactId )
+  return result || null;
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function removeContact(contactId) {
+  
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+ async function addContact(name, email, phone) {
+  const data = await fs.readFile(contactsPath, "utf-8")
+  const contacts = JSON.parse(data);
+const newContact = {
+  id: nanoid(),
+  name,
+  email, 
+  phone,
+}
+contacts.push(newContact);
+await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+return newContact;
+  // ...твій код. Повертає об'єкт доданого контакту. 
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+}
--- index.js
+++ index.js
@@ -2,3 +2,43 @@
 
 const contacts = require("./contacts")
 
+// index.js
+// const argv = require('yargs').argv;
+
+// TODO: рефакторити
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case 'list':
+            const allContacts = await contacts.listContacts();
+            console.log(allContacts);
+            // ...
+            break;
+
+        case 'get':
+            const oneContact = await contacts.getContactById(id);
+            return console.log(oneContact);
+            // ... id
+            break;
+
+        case 'add':
+            const addNewContact = await contacts.addContact(name, email, phone);
+            console.log(addNewContact)
+            // ... name email phone
+            break;
+
+        case 'remove':
+            const deleteContact = await contacts.removeContact(id)
+            console.log(deleteContact)
+            // ... id
+            break;
+
+        // default:
+        //     console.warn('\x1B[31m Unknown action type!');
+    }
+}
+
+// invokeAction(argv);
+
+// invokeAction({action: "list"});
+
+invokeAction({action: "remove", id: "t030w43MA1PMFyzdQY5zM"});
